16c710f21bbdca0626905f9b2c672aa8341d77ff
izzyjhuang <izzyjhuang@gmail.com>
2024-11-03 17:15:25 -0800

    polished front page

--- src/Footer.js
+++ src/Footer.js
@@ -1,36 +1,27 @@
 // Footer.js
 
 import React from 'react';
-import './footer.css'; // Assuming you store the CSS in Footer.css
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesome
-import { faThreads, faXTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import {faEnvelope} from '@fortawesome/free-solid-svg-icons'; // For non-branded icons
+import './footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
     <div className="footer-container">
+      <h1 className="footer-header">Contact Us</h1>
       <div className="footer-icons">
-      <a href="https://linkedin.com/in/izzyhuang" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faLinkedin} /> 
-          <span>LinkedIn</span>
-        </a>
-        <a href="https://x.com/izzyjhuang" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faXTwitter} />
-          <span>Twitter</span>
-        </a>
-        <a href="https://www.threads.net/@izh_27" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faThreads} />
-          <span>Threads</span>
-        </a>
-        <a href="mailto:izzythebroker@gmail.com">
+        <a href="mailto:lavinialeemusicstudio@gmail.com">
           <FontAwesomeIcon icon={faEnvelope} /> 
-          <span>Email</span>
+          <span>Email: lavinialeemusicstudio@gmail.com</span>
+        </a>
+        <a href="tel:+8476519724">
+          <FontAwesomeIcon icon={faPhone} /> 
+          <span>Phone: +1 (847) 651-9724</span>
         </a>
       </div>
-
-        <div className="footer-links">
-            <h5>© Copyright 2024 Lavinia Music Studio</h5>
-        </div>
+      <div className="footer-links">
+        <h5>© Copyright 2024 Lavinia Music Studio</h5>
+      </div>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -24,7 +24,27 @@ const Homepage = ({ selectedLanguage }) => {
     mission_statement: {
         English: " Mission Statement",
         中文: "教學宗旨"
-    }
+    },
+    v_lessons: {
+      English: " Violin/Viola Lessons", 
+      中文: "小提琴/中提琴課"
+    },
+    p_lessons: {
+      English: " Piano Lessons", 
+      中文: "鋼琴課"
+    },
+    home_descriptions: {
+      English: " I offer private piano and violin lessons... once a week in a one-on-one setting...", 
+      中文: "我提供私人鋼琴和小提琴課程... 每週一次，一對一授課..."
+    },
+    performance_opportunities: {
+      English: "Performance Opportunities",
+      中文: "表演機會",
+    },
+    locations: {
+      English: "Serving ... areas",
+      中文: "服務區域",
+    },
   };
 
   const text0 = textTranslations.studio_name[selectedLanguage] || textTranslations.studio_name['English'];
@@ -32,8 +52,12 @@ const Homepage = ({ selectedLanguage }) => {
   const text2 = textTranslations.quote[selectedLanguage] || textTranslations.quote['English'];
   const text3 = textTranslations.learn_more[selectedLanguage] || textTranslations.learn_more['English'];
   const text4 = textTranslations.mission_statement[selectedLanguage] || textTranslations.quote['English'];
-//   const text5 = textTranslations.learn_more[selectedLanguage] || textTranslations.learn_more['English'];
-  
+  const text5 = textTranslations.v_lessons[selectedLanguage] || textTranslations.quote['English'];
+  const text6 = textTranslations.p_lessons[selectedLanguage] || textTranslations.quote['English'];
+  const text7 = textTranslations.home_descriptions[selectedLanguage] || textTranslations.quote['English'];
+  const text8 = textTranslations.performance_opportunities[selectedLanguage] || textTranslations.quote['English'];
+  const text9 = textTranslations.locations[selectedLanguage] || textTranslations.quote['English'];
+
   return (
     <div>
       <div className="banner-container">
@@ -42,18 +66,44 @@ const Homepage = ({ selectedLanguage }) => {
           <div className="banner-image-container">
             <img src={`${baseURL}/images/home/playing-piano.jpeg`} alt="Cover-pic" className="banner-image"/>
           </div>
-          {/* <div className="banner-header-text">
-            <h1>{text1}</h1>
-            <h2>{text2}</h2>
-            <h3>{text3}</h3>
-        </div> */}
+
         </div>
         <div className="mission-section">
-            <img src={`${baseURL}/images/teacher-pic.jpeg`}/>
+            <img src={`${baseURL}/images/home/teacher-pic.jpg`}/>
             <div className="mission-statement">
                 <h1>{text4}</h1>
                 <h2>~Lavinia </h2>
             </div>
+        </div>
+        <div className="lessons-container">
+          <div className="lessons-section">
+              <div className="mission-statement">
+                  <h1>{text5}</h1>
+              </div>
+              <img src={`${baseURL}/images/home/teacher-pic.jpg`}/>
+          </div>
+          <div className="lessons-section">
+              <div className="mission-statement">
+                  <h1>{text6}</h1>
+              </div>
+              <img src={`${baseURL}/images/home/teacher-pic.jpg`}/>
+          </div>
+        </div>
+        <duv className="offering-statement">
+          <div className="mission-statement">
+              <h1>{text7}</h1>
+          </div>
+        </duv>
+        <div className="offering-statement">
+          <div className="mission-statement">
+          <h1 style={{ marginBottom: '2vw' }}>{text8}</h1>
+          </div>
+          <img style={{ width: '80%' }}src={`${baseURL}/images/home/playing-piano.jpeg`}/>
+        </div>
+        <div className="offering-statement">
+          <div className="mission-statement">
+          <h1 style={{ marginBottom: '2vw' }}>{text9}</h1>
+          </div>
         </div>
       </div>
 
